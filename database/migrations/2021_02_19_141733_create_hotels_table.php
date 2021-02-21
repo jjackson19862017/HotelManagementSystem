38eea598d1014d17cefd098ad022775dcc08ca36
Stephen Jackson <jjackson19862017@users.noreply.github.com>
2021-02-21 02:57:12 +0000

    Back to where was originally, before messing up the roles and permission pivot tables

---
+++ database/migrations/2021_02_19_141733_create_hotels_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('telephone')->unique();
+            $table->string('address')->unique();
+            $table->string('town');
+            $table->string('county');
+            $table->string('postcode');
+            $table->string('website');
+            $table->string('email');
+            $table->integer('numberOfRooms');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+}
